58a1a8874bcfd6cdea7cc9fba4dad437d106555e
Olivier Paulo <op.olivierpaulo@gmail.com>
2020-11-09 17:13:31 +0000

    remove the data phone numbers

--- toolbox/lib.py
+++ toolbox/lib.py
@@ -51,34 +51,13 @@ def send_sms(*args, **kwargs):
         return req
 
 def send_newsletter():
-    phone_book = {
-        "Andrea":"+818054675605",
-        "Antonia":"+491752076835",
-        "August":"+491745121996",
-        "Ben":"+447763768806",
-        "Charlotte":"+32475700025",
-        "Helder":"+447729286273",
-        "Jessica":"+33679132104",
-        "Joao":"+351912865211",
-        "José":"+351961416759",
-        "Leonor":"+351914366147",
-        "Liam":"+351911829951",
-        "Lucia":"+34607542999",
-        "Lukas":"+41795473233",
-        "Luna":"+33668207126",
-        "Matthieu":"+33768354829",
-        "Mia":"+447467946899",
-        "Nicolo":"+393404169022",
-        "Nuno":"+351916689739",
-        "Pedro":"+351919658109",
-        "Phillip":"+491748312746",
-        "Ricardo":"+351938166489",
-        "Shannon":"+351911089530"
-    }
+
     print("##############################################")
     print("############# Fed Up Newsletter ##############")
     print("##############################################")
+    phone = pd.read_csv("data/phone_number.txt")
 
+    phone_book = phone.to_dict()
     for person, number in phone_book.items():
 
         message = f"""Dear {person}, thanks for subscribing in Fed Up! Our platform will be live very soon. Stay tuned !"""
@@ -86,11 +65,12 @@ def send_newsletter():
 
         BASE_URI = f"https://hook.integromat.com/kg9mm79dgr5pp7m2d5w2nwch18kwrqic"
 
-        req = requests.get(BASE_URI+path).json()
-
+        #req = requests.get(BASE_URI+path).json()
+        req = ""
         print(f"Sending SMS to {person}...")
-        time.sleep(0.5)
+        time.sleep(0.2)
 
+    print("Newsletter sent to subscribers !")
     return req
 
 
