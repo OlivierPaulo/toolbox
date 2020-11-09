b364131eac985c7ed3370e3b28c10e0ca633bf52
Olivier Paulo <op.olivierpaulo@gmail.com>
2020-11-09 16:49:02 +0000

    Adding Toolbox correction

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='toolbox',
       test_suite = 'tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/toolbox-run','scripts/send_sms'],
+      scripts=['scripts/toolbox-run','scripts/send_sms','scripts/send_newsletter'],
       zip_safe=False)
--- toolbox/lib.py
+++ toolbox/lib.py
@@ -7,6 +7,7 @@ from os.path import split
 import pandas as pd
 import datetime
 import requests
+import time
 
 pd.set_option('display.width', 200)
 
@@ -48,6 +49,49 @@ def send_sms(*args, **kwargs):
         print("\nSending the SMS...\n")
         #print(req)
         return req
+
+def send_newsletter():
+    phone_book = {
+        "Andrea":"+818054675605",
+        "Antonia":"+491752076835",
+        "August":"+491745121996",
+        "Ben":"+447763768806",
+        "Charlotte":"+32475700025",
+        "Helder":"+447729286273",
+        "Jessica":"+33679132104",
+        "Joao":"+351912865211",
+        "José":"+351961416759",
+        "Leonor":"+351914366147",
+        "Liam":"+351911829951",
+        "Lucia":"+34607542999",
+        "Lukas":"+41795473233",
+        "Luna":"+33668207126",
+        "Matthieu":"+33768354829",
+        "Mia":"+447467946899",
+        "Nicolo":"+393404169022",
+        "Nuno":"+351916689739",
+        "Pedro":"+351919658109",
+        "Phillip":"+491748312746",
+        "Ricardo":"+351938166489",
+        "Shannon":"+351911089530"
+    }
+    print("##############################################")
+    print("############# Fed Up Newsletter ##############")
+    print("##############################################")
+
+    for person, number in phone_book.items():
+
+        message = f"""Dear {person}, thanks for subscribing in Fed Up! Our platform will be live very soon. Stay tuned !"""
+        path = f"?number={number}&message={message}"
+
+        BASE_URI = f"https://hook.integromat.com/kg9mm79dgr5pp7m2d5w2nwch18kwrqic"
+
+        req = requests.get(BASE_URI+path).json()
+
+        print(f"Sending SMS to {person}...")
+        time.sleep(0.5)
+
+    return req
 
 
 if __name__ == '__main__':
