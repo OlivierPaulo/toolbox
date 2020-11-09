ace93506a56c47a504769357648fdb9c86a3ddd5
Olivier Paulo <op.olivierpaulo@gmail.com>
2020-11-09 21:25:37 +0000

    Adding test for sms toolbox

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -6,5 +6,11 @@ import toolbox
 import pandas as pd
 # Import from our lib
 import pytest
+from toolbox.lib import send_sms
+from toolbox.lib import send_newsletter
 
+def test_send_sms():
+    assert send_sms({"person":"Olivier", "number":"+33627644637"}) != None
 
+def send_sms():
+    assert send_sms({"person":"Olivier", "number":"+33627644637"}) != None
