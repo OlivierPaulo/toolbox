ee70666177df878904f3289ac8342e854eb3e806
Olivier Paulo <op.olivierpaulo@gmail.com>
2020-11-09 17:54:52 +0000

    update the test

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -9,15 +9,8 @@ from toolbox.lib import clean_data
 import pytest
 
 
-def test_clean_data():
-    datapath = os.path.dirname(os.path.abspath(toolbox.__file__)) + '/data'
-    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
-    first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
-    assert list(df.columns)[:6] == first_cols
-    assert df.shape == (999, 142)
-    out = clean_data(df)
-    assert out.shape == (985, 119)
-
-
 # def test_send_sms():
-#     assert(test_send_sms()) == ""
+#    assert(test_send_sms()) == """Thanks for using our services :).
+# Best wishes,
+# Fed Up !
+# Customer Relations Team"""
